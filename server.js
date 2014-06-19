7623e37958b75b0420b06759ba7ffeee66c0eb35
Ante Lucic <alucic@extensionengine.com>
2014-06-19 14:20:54 -0400

    serve static files

--- server.js
+++ server.js
@@ -1,63 +1,106 @@
 var http = require('http'),
     url  = require('url'),
-    _    = require('underscore');
+    fs   = require('fs'),
+     _   = require('underscore');
+ 
+// serving static files
+function getStaticFile(filePath, res){
 
-console.log('Server running...');
+  fs.exists(filePath, function(exists){
 
-http.createServer(function (req, res) {
-
-  var query = url.parse(req.url).search;
-
-  if (query) {
-    fetchMarketData(query, function (result) {
-      res.writeHead(200, {
-        'Content-Type': 'application/json',
-        'Content-Length': result.length,
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+    if(exists){
+      fs.readFile(filePath, function(err, contents){
+        if(!err){
+          res.end(contents);
+        } else {
+          console.log(err);
+        };
       });
+    } else {
+      //serve-up 404 page
+      fs.readFile(__dirname + '/app/404.html', function(err, contents){
+        if(!err){
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          res.end(contents);
+        } else {
+          console.log(err);
+        }
+      });
+    }
+  });
+}
 
-      res.write(result);
-      res.end();
-    });
-  } else {
-    res.writeHead(404, {'Content-Type':'application/json'});
-    res.write('{"error":"Wrong!"}');
-    res.end();
-  }
+// returns JSON data
+function getMarketData(query, res) {
 
-}).listen(1338, '0.0.0.0');
+  //TODO: Validate query
+  query = query || '?borough=1';
 
-function fetchMarketData(query, callback) {
   var payload = '',
       result  = [];
 
   var options = {
-    host: 'www.grownyc.org',
-    port: 80,
-    path: '/greenmarket.php' + query,
-    method: 'GET'
-  };
+        host: 'www.grownyc.org',
+        port: 80,
+        path: '/greenmarket.php' + query,
+        method: 'GET'
+      };
 
-  var req = http.request(options, function(res) {
-    res.setEncoding('utf8');
+  var headers = {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+      };
 
-    res.on('data', function (chunk) {
+  // rename datares to something better?
+  var req = http.request(options, function(datares) {
+    datares.setEncoding('utf8');
+
+    datares.on('data', function (chunk) {
       payload += chunk;
     });
 
-    res.on('end', function() {
+    datares.on('end', function() {
        _.each(JSON.parse(payload).sites, function(market) {
         result.push(_.omit(market,'infoBubbleHTML'));
       });
 
-      callback(JSON.stringify(result));
+      data = JSON.stringify(result);
+      headers['Content-Length'] = data.length;
+
+      // if we get just [] back, something is wrong with request
+      // in future when we validateQuery() we shuld return 500 instead of 400, something is wrong on our end
+      data.length < 3 ? res.writeHead(400, headers) : res.writeHead(200, headers);
+      
+      res.write(data);
+      res.end();
     });
   });
 
   req.on('error', function(e) {
-    console.log('problem with request: ' + e.message);
+    console.log('Error fetching data: ' + e.message);
   });
 
   req.end();
 }
+
+// handle HTTP requests
+function requestHandler(req, res) {
+  var uri         = url.parse(req.url),
+      fileName    = uri.pathname.substring(1) || 'index.html',
+      localFolder = __dirname + '/app/';
+
+  switch(uri.pathname) {
+    case '/api':
+      getMarketData(uri.search, res);
+      break;
+    default:
+      getStaticFile((localFolder + fileName), res);
+      break;
+  }
+}
+
+http.createServer(requestHandler)
+.listen(3000);
+
+console.log('Server running at http://localhost:3000/');
